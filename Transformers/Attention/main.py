f46dc14655338221189ee485ec7b7467c7493c39
Eshaan111 <eshaanbansal2005@gmail.com>
2026-09-05 17:39:00 +0530

    MASKED ATTENTION

--- Transformers/Attention/main.py
+++ Transformers/Attention/main.py
@@ -34,19 +34,20 @@ print(os.getcwd(), device)
 
 class MultiHeadedAttention(nn.Module):
 
-    def __init__(self,num_heads,input_embedding_dim, device):
+    def __init__(self,num_heads,input_embedding_dim, device, masked=False, ):
         super().__init__()
 
         if(input_embedding_dim%num_heads != 0):
             raise ValueError(f"CANT DIVIDE Embedding-dim{input_embedding_dim} into Number-Heads{num_heads}")
 
         embedding_dim = input_embedding_dim//num_heads # Eh
+        self.isMasked = masked
         self.device = device
         self.num_heads = num_heads
         self.expected_dim = input_embedding_dim 
-        self.tensor_all_query = nn.Parameter(torch.rand((num_heads, input_embedding_dim, embedding_dim), device=self.device))#H,B,T,Eh
-        self.tensor_all_key = nn.Parameter(torch.rand((num_heads, input_embedding_dim, embedding_dim), device=self.device))#H,B,T,Eh
-        self.tensor_all_value = nn.Parameter(torch.rand((num_heads, input_embedding_dim, embedding_dim), device=self.device))#H,B,T,Eh
+        self.tensor_all_query = nn.Parameter(torch.rand((num_heads, input_embedding_dim, embedding_dim), device=self.device))#H,E,Eh
+        self.tensor_all_key = nn.Parameter(torch.rand((num_heads, input_embedding_dim, embedding_dim), device=self.device))#H,E,Eh
+        self.tensor_all_value = nn.Parameter(torch.rand((num_heads, input_embedding_dim, embedding_dim), device=self.device))#H,E,Eh
         self.output_weights = nn.Parameter(torch.rand((input_embedding_dim,input_embedding_dim), device=self.device)) #E,E
 
 
@@ -69,9 +70,16 @@ class MultiHeadedAttention(nn.Module):
             key_embeds = batch @ key_tensor 
             value_embeds = batch @ value_tensor
 
-            temp = query_embeds @ key_embeds.permute(0,2,1) # BxTxEh . BxEhxT = BxTxT
+            temp = query_embeds @ key_embeds.permute(0,2,1) # BxTxEh . BxEhxT = BxTqueyxTkey
             scaled = temp/sqrt(embedding_dim)
 
+            if self.isMasked:
+                idx_T_row = torch.arange(timestep_count, device=device).view(timestep_count,1)
+                idx_T_col = torch.arange(timestep_count, device=device).view(1,timestep_count)
+                mask_0 = idx_T_col <= idx_T_row
+                mask_inf = idx_T_col > idx_T_row
+                scaled[:,mask_0] += 0
+                scaled[:,mask_inf] = float("-inf")
             head_output_embeds = torch.softmax(scaled, dim=2 ) @ value_embeds
 
             
